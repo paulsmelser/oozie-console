f3c2c348a51587a41db947e6a4088b24dbf2abbb
psmelser <paul.smelser@gmail.com>
2016-06-22 23:43:40 -0400

    no idea

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,17 @@
 #!/usr/bin/env bash
 
+sudo su
+
 apt-get update
 
 apt-get install -y nginx
 apt-get install -y vim
 
+apt-get install python-software-properties
+add-apt-repository ppa:webupd8team/java
+apt-get update
+apt-get install oracle-java8-installer
 
-sudo service nginx start
+service nginx start
+
+exit
